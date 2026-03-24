360f77062611cec57bd2a7510d671f39a6e9484b
Jaylen-Watkins <Jaylenw2@gmail.com>
2026-03-24 14:41:36 -0400

    Added ability to take User Input in CLI

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,7 +17,7 @@ client = genai.Client(api_key=api_key)
 
 response =client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents= args.user_prompt
 )
 if response.usage_metadata == None:
     raise RuntimeError("Usage metadata is missing from the response.")
